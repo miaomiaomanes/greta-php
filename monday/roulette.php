5ec87e8eaa054e26970611d64a93ac93e59c1b3e
miaomiao <895489555@qq.com>
2022-11-14 09:36:03 +0100

    first commit

---
+++ monday/roulette.php
@@ -0,0 +1,20 @@
+<?php
+
+
+function gagne($num,$typeEuro)
+{
+    $resultat = rand(0, 36);
+    $is_odd = $resultat % 2; 
+    
+    echo "les resulatat random est" ." " . $resultat . " " . ($is_odd ? 'impair' : 'pair') . "/n";
+
+    if ($num === $resultat) {
+        echo " vous avez gagne" . " " . $typeEuro;
+    } else {
+        echo " vous avez perdu" . " " . $typeEuro;
+    }
+}
+
+$typeNumber = intval($argv[1]);
+$typeEuro=intval($argv[2]);
+gagne($typeNumber,$typeEuro);
